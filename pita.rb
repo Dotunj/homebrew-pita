fdfc389214e13b5e912c46b6cd492b87504985d7
dotunj <dotunj@gmail.com>
2022-01-24 08:03:58 +0100

    Brew formula update for pita version v0.0.8

--- pita.rb
+++ pita.rb
@@ -5,26 +5,42 @@
 class Pita < Formula
   desc "Binary Distribution for Pita"
   homepage "https://github.com/dotunj/homebrew-pita"
-  version "0.0.7"
+  version "0.0.8"
 
   on_macos do
-    if Hardware::CPU.intel?
-      url "https://github.com/Dotunj/pita/releases/download/v0.0.7/homebrew-pita_0.0.7_darwin_x86_64.tar.gz"
-      sha256 "c8504c504cdbd167ef6608d4b099b29a6c48e62faa6584400d60cc7e0c8e1409"
+    if Hardware::CPU.arm?
+      url "https://github.com/Dotunj/pita/releases/download/v0.0.8/pita_0.0.8_darwin_arm64.tar.gz"
+      sha256 "ff9ac9e31eff8ced89503ee68d48de6354aa6dedaf7e06bce4c5be23b8a3c43a"
 
       def install
-        bin.install "homebrew-pita"
+        bin.install "pita"
+      end
+    end
+    if Hardware::CPU.intel?
+      url "https://github.com/Dotunj/pita/releases/download/v0.0.8/pita_0.0.8_darwin_x86_64.tar.gz"
+      sha256 "f9e570abe52f5474787b450b1fc153e60576ed8d378b29e5bfcde5343c2665f6"
+
+      def install
+        bin.install "pita"
       end
     end
   end
 
   on_linux do
-    if Hardware::CPU.intel?
-      url "https://github.com/Dotunj/pita/releases/download/v0.0.7/homebrew-pita_0.0.7_linux_x86_64.tar.gz"
-      sha256 "5e6d81070b4ec41121313fe963cc81950a2751190a7ed37215af9392406deebc"
+    if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
+      url "https://github.com/Dotunj/pita/releases/download/v0.0.8/pita_0.0.8_linux_arm64.tar.gz"
+      sha256 "7f3887c9550f5938cca00a3f949e0d0dcb83267c4a012d4d142daca1912ca78a"
 
       def install
-        bin.install "homebrew-pita"
+        bin.install "pita"
+      end
+    end
+    if Hardware::CPU.intel?
+      url "https://github.com/Dotunj/pita/releases/download/v0.0.8/pita_0.0.8_linux_x86_64.tar.gz"
+      sha256 "961fdf1c05b9ee07a6a692d3e81d7203bb0a0cfee40333e39fb2919189e210c3"
+
+      def install
+        bin.install "pita"
       end
     end
   end
